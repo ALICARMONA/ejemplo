6bb358a71918e29441eb4806038c5a8147f95b4b
Ernesto Medina <ernesto.m.medina.m@gmail.com>
2020-08-03 20:03:31 +0000

    cards example

---
+++ src/js/component/Card.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const Card = ({ name, color = "lightgray" }) => {
+	return (
+		<div
+			className={"c" + "a" + "r" + "d" + " mx-auto my-4"}
+			style={{
+				width: "18rem",
+				backgroundColor: color,
+				color: "white"
+			}}>
+			<img
+				src="..."
+				className="card-img-top"
+				alt="imagen de la persona"
+			/>
+			<div className="card-body">
+				<h5 className="card-title">{name}</h5>
+				<p className="card-text">
+					{`Soy el profesor de esta cohorte.`}
+				</p>
+				<a href="#" className="btn btn-primary">
+					Go somewhere
+				</a>
+			</div>
+		</div>
+	);
+};
+
+Card.propTypes = {
+	name: PropTypes.string.isRequired,
+	color: PropTypes.string
+};
